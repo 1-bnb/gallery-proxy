f90e729059cd84f76f8006dabe1aee244fc47940
cho319 <cho319us@gmail.com>
2020-08-19 00:02:32 -0700

    Connected to Calendar Component

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Harry-Airbnb-proxy
+
+> Proxy server for the clone of Airbnb components
+
+## Related Projects
+
+  - https://github.com/7LuckyAirbnboiiz/gallery
+  - https://github.com/7LuckyAirbnboiiz/calendar
+  - https://github.com/7LuckyAirbnboiiz/reviews-section
+  - https://github.com/7LuckyAirbnboiiz/image_carousel
+
+## Table of Contents
+
+1. [Usage](#Usage)
+1. [Requirements](#requirements)
+1. [Development](#development)
+
+## Usage
+
+> Some usage instructions
+
+## Requirements
+
+An `nvmrc` file is included if using [nvm](https://github.com/creationix/nvm).
+
+- Node 6.13.0
+- etc
+
+## Development
+
+### Installing Dependencies
+
+From within the root directory:
+
+```sh
+npm install -g webpack
+npm install
+```
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // Dependency
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const app = express();
 const PORT = 3000;
 
@@ -9,7 +10,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-// Route
+/* Route for Photo Gallery Component */
+
+/* Route for Calendar Component */
+
+// GET request to '/rooms/:room_id/reservation' route
+app.get('/rooms/:room_id/reservation', (req, res) => {
+  let room_id = req.params.room_id;
+  axios.get(`http://localhost:3002/rooms/${room_id}/reservation`)
+    .then(response => {
+      res.status(200).send(response.data);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
+
+// POST request to '/rooms/:room_id/reservation' route
+app.post('/rooms/:room_id/reservation', (req, res) => {
+  let room_id = req.params.room_id;
+  // declare the reservation data to post
+  let reservation = {
+    check_in: req.body.check_in,
+    check_out: req.body.check_out
+  }
+  axios.post(`http://localhost:3002/rooms/${room_id}/reservation`, reservation)
+    .then(response => {
+      res.status(200).send(response.data);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
+});
+
+/* Route for Reviews Component */
+
+/* Route for Image Carousel Component */
 
 // Start server
 app.listen(PORT, () => {
